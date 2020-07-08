a0f3572123c2a97a000c16cff13a6effa57b7c4c
ddri <david@paratags.com>
2020-07-08 13:20:03 +1000

    Add comments

--- walker.js
+++ walker.js
@@ -1,9 +1,13 @@
 var w;
 
+// Create the canvas
+
 function setup() {
   createCanvas(640, 360);
   w = new Walker();
 }
+
+// Create the objects and actions  
 
 function draw() {
   background(51);
@@ -11,15 +15,19 @@ function draw() {
   w.display();
 }
 
+// Create the walker. 
+
 function Walker() {
   this.x = width/2;
   this.y = height/2;
   
+  // Create the action for Walker including random functions
   this.walk = function() {
     this.x = this.x + random(-5, 5);
     this.y = this.y + random(-5, 5);
   }    
   
+  // Draw the walker
   this.display = function() {
     fill(255);
     ellipse(this.x, this.y, 48, 48);
