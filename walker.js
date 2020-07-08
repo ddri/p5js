9d2705b4949d3138de4ef4e0610dd1ec92c984df
David Ryan <david@corilla.com>
2020-07-08 12:46:39 +1000

    Create walker.js

---
+++ walker.js
@@ -0,0 +1,21 @@
+var w;
+
+function setup() {
+  createCanvas(640, 360);
+  w = new Walker();
+}
+
+function draw() {
+  background(51);
+  w.display();
+}
+
+function Walker() {
+  this.x = width/2;
+  this.y = height/2;
+  
+  this.display = function() {
+    fill(255);
+    ellipse(this.x, this.y, 48, 48);
+  }
+}
