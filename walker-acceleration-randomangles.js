5098915a21bd3cc76a7a18bffd9bcfb6ab485e27
ddri <david@paratags.com>
2020-07-09 11:51:27 +1000

    Set acceleration at time of creation

--- walker-acceleration-randomangles.js
+++ walker-acceleration-randomangles.js
@@ -16,17 +16,21 @@ function draw() {
 }
 
 // Create the walker
+// The acceleration is now set at time of creation rather 
+// than as an update. Magnitude is scaled back also.
 
 function Walker() {
   this.pos = createVector(width/2, height/2);
   this.vel = createVector(0, 0);
+  this.acc = p5.Vector.fromAngle(random(0, TWO_PI))
+  this.acc.setMag(0.2);
 
   this.update = function() {
 
     // This exampel is purely to explore direction specific by Pi
     // in the fromAngle function. And learn how to set it
     // randomly.
-    this.acc = p5.Vector.fromAngle(random(0, TWO_PI))
+    this.acc.rotate(0.1);
     this.vel.add(this.acc);
     this.pos.add(this.vel);
   }    
