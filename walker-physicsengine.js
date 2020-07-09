21c2c4e39340142a2b3768d3e33b187dd82690ce
ddri <david@paratags.com>
2020-07-09 17:08:10 +1000

    Fix typo

--- walker-physicsengine.js
+++ walker-physicsengine.js
@@ -20,7 +20,7 @@ function draw() {
 function Walker() {
   this.pos = createVector(width/2, 0);
   this.vel = createVector(0, 0);
-  this.acc = createVector(0, 0.1)''
+  this.acc = createVector(0, 0.1);
 
   
   // Create the action for Walker including random functions
