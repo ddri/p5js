b4144b9ef9e09a2011621af64a71ab69515066db
ddri <david@paratags.com>
2020-07-17 18:07:44 +1000

    Refactor with liquid
    Drag force overhaul with liquid example to show mass affected by differences in friction.

--- session-2-vector-and-forces/sketch-06-designing-friction.js
+++ session-2-vector-and-forces/sketch-06-designing-friction.js
@@ -1,104 +1,146 @@
+// Drag force
 
-// First section is the environment
-
+// Two particles, this could be an array
 var particle1;
 var particle2;
 
+// Liquid
+var liquid;
+
 function setup() {
-    createCanvas(640, 360);
-    particle1 = new Particle(200, 100, 3);
-    particle2 = new Particle(400, 100, 1);
-}
 
-// This function prints to console, to show the 
-// subtle floating point math errors that cause 
-// the two particles to behave different with 
-// similar gravity and wind calculations
-function mousePressed() {
-    console.log(particle1.vel.y + "  " + particle2.vel.y);
-}
+  createCanvas(640, 360);
+  // Create liquid object
+  liquid = new Liquid(0, height / 2, width, height / 2, 0.1);
 
+  particle1 = new Particle(1, 200, 0);
+  particle2 = new Particle(3, 400, 0);
+}
 
 function draw() {
-    background(51);
+  background(127);
 
-    // This variable applies gravity and wind as a force
+  // Draw water
+  liquid.display();
 
-    var wind = createVector(0.1, 0);
+  // Is the Mover in the liquid?
+  if (liquid.contains(particle1)) {
+    // Calculate drag force
+    var dragForce = liquid.calculateDrag(particle1);
+    // Apply drag force to Mover
+    particle1.applyForce(dragForce);
+  }
 
-    var gravity1 = createVector(0, 0.2*particle1.mass); 
-    particle1.applyForce(gravity1);    
-    var gravity2 = createVector(0, 0.2*particle2.mass); 
-    particle2.applyForce(gravity2);
+  // Gravity is scaled by mass here!
+  var gravity = createVector(0, 0.1 * particle1.mass);
+  // Apply gravity
+  particle1.applyForce(gravity);
+
+  // Update and display
+  particle1.update();
+  particle1.display();
+  particle1.checkEdges();
 
 
-    // The mouseIsPressed condition applies the 
-    // wind force only if the mouse is clicked
+  // Is the Mover in the liquid?
+  if (liquid.contains(particle2)) {
+    // Calculate drag force
+    var dragForce = liquid.calculateDrag(particle2);
+    // Apply drag force to Mover
+    particle2.applyForce(dragForce);
+  }
 
-    if (mouseIsPressed) {
-    particle1.applyForce(wind);
-    particle2.applyForce(wind);    
-    }
+  // Gravity is scaled by mass here!
+  var gravity = createVector(0, 0.1 * particle2.mass);
+  // Apply gravity
+  particle2.applyForce(gravity);
 
-    // could use a random to create drift in the fall
-    // instead of the set x value
-    // var gravity = createVector(random(-1,1),0.1)
-
-    particle1.update();
-    particle1.edges();
-    particle1.display();
-    particle2.update();
-    particle2.edges();
-    particle2.display();  
+  // Update and display
+  particle2.update();
+  particle2.display();
+  particle2.checkEdges();
 }
 
 
-// Second section is the particle physics
+// Particles
 
-function Particle(x, y, m) {
-    this.pos = createVector(x, y);
-    this.vel = createVector(0, 0);
-    this.acc = createVector(0, 0);
-    this.mass = m;
 
-    this.applyForce = function(force) {
-        var f = force.copy();
-        f.div(this.mass);
-        this.acc.add(f);
+function Particle(m,x,y) {
+  this.mass = m;
+  this.position = createVector(x,y);
+  this.velocity = createVector(0,0);
+  this.acceleration = createVector(0,0);
+
+  // Newton's 2nd law: F = M * A
+  // or A = F / M
+  this.applyForce = function(force) {
+    var f = p5.Vector.div(force,this.mass);
+    this.acceleration.add(f);
+  };
+
+  this.update = function() {
+    // Velocity changes according to acceleration
+    this.velocity.add(this.acceleration);
+    // position changes by velocity
+    this.position.add(this.velocity);
+    // We must clear acceleration each frame
+    this.acceleration.mult(0);
+  };
+
+  this.display = function() {
+    stroke(0);
+    strokeWeight(2);
+    fill(255,127);
+    ellipse(this.position.x,this.position.y,this.mass*16,this.mass*16);
+  };
+
+  // Bounce off bottom of window
+  this.checkEdges = function() {
+    if (this.position.y > height) {
+      this.velocity.y *= -0.9;  // A little dampening when hitting the bottom
+      this.position.y = height;
     }
+  };
 
-    this.update = function() {
-        this.vel.add(this.acc);
-        this.pos.add(this.vel);
-        this.acc.set(0, 0);
-
-        // Adding friction by reducing the velocity
-        // which will make the particles look like 
-        // they are bouncing and losing momentum as
-        // they do
-        this.vel.mult(0.98);
-
-    }
-
-    this.display = function() {
-        fill(255);
-        stroke(255);
-        ellipse(this.pos.x, this.pos.y, this.mass*10, this.mass*10,);
-    }
-    // This tells the particle to react to the edges
-    // of the canvas, and what it should do when it 
-    // hits it. In this case, to reverse the vector.
-
-    this.edges = function() {
-        if (this.pos.y > height) {
-            this.vel.y *= -1;
-            this.pos.y = height;
-        }
-
-         if (this.pos.x > width) {
-            this.vel.x *= -1;
-            this.pos.x = width;
-        }
-
-    }
 }
+
+
+// Liquid
+
+
+var Liquid = function(x, y, w, h, c) {
+  this.x = x;
+  this.y = y;
+  this.w = w;
+  this.h = h;
+  this.c = c;
+
+  // Is the Mover in the Liquid?
+  this.contains = function(m) {
+    var l = m.position;
+    return l.x > this.x && l.x < this.x + this.w &&
+           l.y > this.y && l.y < this.y + this.h;
+  };
+
+  // Calculate drag force
+  this.calculateDrag = function(m) {
+    // Magnitude is coefficient * speed squared
+    var speed = m.velocity.mag();
+    var dragMagnitude = this.c * speed * speed;
+
+    // Direction is inverse of velocity
+    var dragForce = m.velocity.copy();
+    dragForce.mult(-1);
+
+    // Scale according to magnitude
+    dragForce.setMag(dragMagnitude);
+    return dragForce;
+  }
+
+  this.display = function() {
+    noStroke();
+    fill(50);
+    rect(this.x, this.y, this.w, this.h);
+  }
+}
+
