d8c130da18949c94e12b042025bce369f8d001fe
ddri <davedri@gmail.com>
2023-04-07 14:49:11 +0800

    Create spinning-circle-001.js

---
+++ general/spinning-circle-001.js
@@ -0,0 +1,58 @@
+// Initialize variables
+let angle = 0; // starting angle of the spinning shape
+let trail = []; // array to store the trail
+
+function setup() {
+  // Create a canvas
+  createCanvas(400, 400);
+  // Set the frame rate to 60fps
+  frameRate(60);
+}
+
+function draw() {
+  // Set the background color with low opacity to create a fading effect
+  background(220, 20);
+
+  // Calculate the position of the center of the canvas
+  let centerX = width / 2;
+  let centerY = height / 2;
+
+  // Calculate the position of the spinning shape based on the current angle
+  let x = centerX + cos(angle) * 100;
+  let y = centerY + sin(angle) * 100;
+
+  // Add the current position to the trail array
+  trail.push({ x, y });
+
+  // Draw the trail
+  noFill();
+  stroke(255, 0, 0); // Set the stroke color to red
+  strokeWeight(2); // Set the stroke weight to 2 pixels
+  beginShape();
+  for (let i = 0; i < trail.length; i++) {
+    // Calculate the alpha value of the trail based on its age
+    let alpha = map(i, 0, trail.length - 1, 255, 0);
+    stroke(255, 0, 0, alpha); // Set the stroke color with fading alpha value
+    // Draw a vertex at the current trail position
+    vertex(trail[i].x, trail[i].y);
+  }
+  endShape();
+
+  // Draw the spinning shape
+  fill(0, 0, 255); // Set the fill color to blue
+  noStroke();
+  ellipse(x, y, 50, 50);
+
+  // Update the angle of the spinning shape
+  angle += 0.05;
+
+  // Remove the oldest point from the trail array if it has been more than 5 seconds
+  if (trail.length > 0 && millis() - trail[0].time > 5000) {
+    trail.shift();
+  }
+}
+
+function mousePressed() {
+  // Clear the trail when the mouse is clicked
+  trail = [];
+}
